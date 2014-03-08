8e41902d95200c6e23c96a0eedabbb459bc09c62
deitrick <ajdeitrick@gmail.com>
2014-03-08 14:08:10 -0700

    created static pages controller and link to login on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Thefirehose::Application.routes.draw do
+  root to: "static_pages#index"
   get   '/login', :to => 'sessions#new', :as => :login
   get '/auth/github/callback', :to => 'sessions#create'
   get '/auth/failure', :to => 'sessions#failure'
