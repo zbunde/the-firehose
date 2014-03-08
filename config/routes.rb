0c86177b71f02bb3d98fc577175356211aea77cf
deitrick <ajdeitrick@gmail.com>
2014-03-08 14:35:07 -0700

    added log out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Thefirehose::Application.routes.draw do
   get   '/login', :to => 'sessions#new', :as => :login
   get '/auth/github/callback', :to => 'sessions#create'
   get '/auth/failure', :to => 'sessions#failure'
+  get '/logout', to: 'sessions#destroy'
 end
