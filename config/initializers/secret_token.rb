2fa9d721b5d4f0b10bf9baf04e297a5cdcf1e62e
deitrick <ajdeitrick@gmail.com>
2014-03-08 12:36:29 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thefirehose::Application.config.secret_key_base = 'bc53af3eb2bb61bb915b95ed1580716d391124148f6e724159246db3ca85c70f24c6f4c78372894c2b681990a053fc85b847423085960575e93f8a7177ffc9ac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Thefirehose::Application.config.session_store :cookie_store, key: '_thefirehose_session'
