83f6562741e216f9f78da0dbf92ffef45255dc39
deitrick <ajdeitrick@gmail.com>
2014-03-08 17:06:23 -0700

    fixing whitespace on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Thefirehose::Application.routes.draw do
   root to: "static_pages#index"
-  get   '/login', :to => 'sessions#new', :as => :login
+  get '/login', :to => 'sessions#new', :as => :login
   get '/auth/github/callback', :to => 'sessions#create'
   get '/auth/failure', :to => 'sessions#failure'
   get '/logout', to: 'sessions#destroy'
