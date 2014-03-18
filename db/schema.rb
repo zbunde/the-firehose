3901c91140d1aaf09300b4455e29c07da405f0b7
Andrew Deitrick <ajdeitrick@gmail.com>
2014-03-18 15:09:33 -0600

    added title to links model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140309001060) do
+ActiveRecord::Schema.define(version: 20140318201135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140309001060) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
   end
 
   create_table "taggings", force: true do |t|
