3467d69491534c268e024195bd9dfdfe59bcb15a
Andrew Deitrick <ajdeitrick@gmail.com>
2014-03-20 17:50:13 -0600

    added tests for link responses

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Link do
-  before { @link = Link.create url: "google.com", user_id: 1, tag}
+  before { @link = Link.create url: "google.com", user_id: 1, title: "This is a link" }
+
+  it "should respond to a user" do
+    expect(@link.user_id).to eq 1
+  end
+
+  it "should respond to a url" do
+    expect(@link.url).to eq "google.com"
+  end
+
+  it "should respond to a title" do
+    expect(@link.title).to eq "This is a link"
+  end
+
 end
