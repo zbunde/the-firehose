7b458816fc6b9c4cbdd965593dc9984012c606a4
Andrew Deitrick <ajdeitrick@gmail.com>
2014-03-21 13:39:20 -0600

    added test for listing all tags in nav bar

--- spec/features/link_index_spec.rb
+++ spec/features/link_index_spec.rb
@@ -1,16 +1,40 @@
 require 'spec_helper'
 
 describe "links?tag=meta" do
-
-  it "should display a link after creating it" do
+  before do
     login
     @user = User.create!(name: "User", email: "user@example.com", provider: "github")
     @authorization = Authorization.create!(uid: "123545", user: @user, provider: "github")
-    @link = Link.create! url: "google.com", tag_list: "meta", user_id: @user.id, title: "I'm a link", description: "This is a description"
+    @link = Link.create! url: "google.com", tag_list: "meta, ruby, search, dude", user_id: @user.id, title: "I'm a link", description: "This is a description"
+  end
+  it "should display a link after creating it" do
     visit "/"
     click_on "Login with Github"
     visit "/links?tag=meta"
     expect(page).to have_content("I'm a link")
     expect(page).to have_content("This is a description")
+  end
+end
+
+describe "/links" do
+  before do
+    login
+    @user = User.create!(name: "User", email: "user@example.com", provider: "github")
+    @authorization = Authorization.create!(uid: "123545", user: @user, provider: "github")
+    @link = Link.create! url: "google.com", tag_list: "meta, ruby, search, dude", user_id: @user.id, title: "I'm a link", description: "This is a description"
+  end
+  it "should display a list of tags" do
+    tags = []
+    Link.all.each do |link|
+      link.tags.each do |tag|
+        tags << tag.name
+      end
+    end
+    visit "/"
+    click_on "Login with Github"
+    expect(page).to have_content("meta")
+    expect(page).to have_content("search")
+    expect(page).to have_content("dude")
+    expect(page).to have_content("ruby")
   end
 end
--- spec/support/capybara_helpers.rb
+++ spec/support/capybara_helpers.rb
@@ -5,7 +5,6 @@ module CapybaraHelpers
     OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
                                                                   :provider => 'github',
                                                                   :uid => '123545'
-                                                                  # etc.
                                                                 })
   end
 end
